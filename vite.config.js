1ac3b8dbed3429fad12cc868c19619fd8328708b
Jaivanth Koppula <jaivanthkoppula999@gmail.com>
2026-09-11 13:58:58 +0530

    Migrate portfolio to Vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    loader: 'jsx', // Treat `.js` files as `.jsx`
-    include: /src\/.*\.js$/, // Apply only to your source files
-  },
+  plugins: [
+    react({
+      include: /\.(js|jsx)$/,
+    }),
+  ],
+
 });
