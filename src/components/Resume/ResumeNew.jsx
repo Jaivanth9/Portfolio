5733a535022e1f42c14efa6a9612ec4e2e9ad205
Jaivanth Koppula <jaivanthkoppula999@gmail.com>
2026-09-11 14:09:16 +0530

    Fix assets and resume PDF

--- src/components/Resume/ResumeNew.jsx
+++ src/components/Resume/ResumeNew.jsx
@@ -2,13 +2,11 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
-
+import pdf from "../../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.mjs`;
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
 
